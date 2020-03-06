796222cb62f139dc7959ef8e634dd2db4f3d003a
LebedenkoAlexey <61521202+LebedenkoAlexey@users.noreply.github.com>
2020-03-06 06:31:09 +0400

    Delete Lesson1.java

--- Lebedenko/Lesson1/Lesson1.java
+++
@@ -1,8 +0,0 @@
-package src.main.java.Lebedenko.Lesson1;
-
-public class Lesson1 {
-    public static void main(String[] args) {
-        Strings strings = new Strings ();
-        strings.selectFromMenu ();
-    }
-}
