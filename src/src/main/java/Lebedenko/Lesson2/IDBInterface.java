4d484f4fe0ba6357e55839e9d501ef098909988e
LebedenkoAlexey <61521202+LebedenkoAlexey@users.noreply.github.com>
2020-03-11 23:59:58 +0400

    Delete IDBInterface.java

--- src/src/main/java/Lebedenko/Lesson2/IDBInterface.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.Lebedenko.Lesson2;
-
-public interface IDBInterface <Human>{
-    Human  getSingleRecordFromDB ();
-    Human getAllRecordFromDB ();
-    void saveSingleRecordToDB (Human human);
-    void saveAllRecordToDB (Human human);
-}
-
